14a2cd08b1f1ced85ed26fe4079f07678314e761
marijn08 <marijn0509@gmail.com>
2025-03-18 11:23:40 +0100

    met unirest

--- Marijn/api-test/src/main/java/je/applicatie/domain/apitest/ApiTestApplication.java
+++ Marijn/api-test/src/main/java/je/applicatie/domain/apitest/ApiTestApplication.java
@@ -1,12 +1,9 @@
 package je.applicatie.domain.apitest;
 
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 @SpringBootApplication
 public class ApiTestApplication {
@@ -14,19 +11,13 @@ public class ApiTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiTestApplication.class, args);
 
-        // API call logic should go inside main or in a method
-        HttpClient client = HttpClient.newHttpClient();
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://booking-com15.p.rapidapi.com/api/v1/attraction/getAttractionReviews?id=PR6K7ZswbGBs&page=1"))
+        HttpResponse<String> response = Unirest.get("https://booking-com15.p.rapidapi.com/api/v1/attraction/getAttractionReviews")
+                .queryString("id", "PR6K7ZswbGBs")
+                .queryString("page", "1")
                 .header("x-rapidapi-host", "booking-com15.p.rapidapi.com")
                 .header("x-rapidapi-key", "91c1fdc560msh2a0d7b58c228e7dp1f0cf2jsn59334af69949")
-                .GET()
-                .build();
+                .asString();
 
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .thenAccept(System.out::println)
-                .join();
+        System.out.println(response.getBody());
     }
 }
