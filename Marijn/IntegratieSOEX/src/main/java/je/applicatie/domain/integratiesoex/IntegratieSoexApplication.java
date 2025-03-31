540c72eb331f1084fa793123960d8d5ed636c05c
marijn08 <marijn0509@gmail.com>
2025-03-31 12:57:59 +0200

    .

---
+++ Marijn/IntegratieSOEX/src/main/java/je/applicatie/domain/integratiesoex/IntegratieSoexApplication.java
@@ -0,0 +1,13 @@
+package je.applicatie.domain.integratiesoex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegratieSoexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegratieSoexApplication.class, args);
+	}
+
+}
---
+++ Marijn/IntegratieSOEX/src/test/java/je/applicatie/domain/integratiesoex/IntegratieSoexApplicationTests.java
@@ -0,0 +1,13 @@
+package je.applicatie.domain.integratiesoex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegratieSoexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
